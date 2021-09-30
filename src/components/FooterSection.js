56b76595dbc928e8e44d9b3cfc13207aa714f4d9
Shola Taiwo <m_webmaster007@yahoo.co.uk>
2021-09-30 18:07:50 +0100

    made modification to the footer section

--- src/components/FooterSection.js
+++ src/components/FooterSection.js
@@ -25,7 +25,7 @@ const FooterSection = () => {
           </li>
         </ul>
         <ul className='social-links'>
-          <li><a href='https://www.facebook.com/taiwo.s.michael.7' target='_blank' rel="noreferrer"> <FaFacebook /></a></li>
+          <li><a href='https://www.facebook.com/' target='_blank' rel="noreferrer"> <FaFacebook /></a></li>
           <li><a href='https://github.com/SmtOmega' target='_blank' rel="noreferrer"> <FaGithub /></a></li>
           <li><a href='https://twitter.com/smtomega' target='_blank' rel="noreferrer"> <FaTwitter /></a></li>
         </ul>
